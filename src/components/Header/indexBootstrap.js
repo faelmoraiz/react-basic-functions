50b885c8831f08b270ac09939f4724a99be55ef8
faelmoraiz <faelmoraiz@gmail.com>
2019-07-04 10:25:36 -0300

    update

--- src/components/Header/indexBootstrap.js
+++ src/components/Header/indexBootstrap.js
@@ -1,29 +1,47 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, Badge } from "react-bootstrap";
 
 import "./Header.css";
 
-export default function Headers() {
-  return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Navbar.Brand as={Link} to="/">
-        ReactJS
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/counter">
-            Contador
-          </Nav.Link>
-          <Nav.Link as={Link} to="/todos">
-            Tarefas
-          </Nav.Link>
-          <Nav.Link as={Link} to="/text">
-            Editor de texto
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+export default class Headers extends React.Component {
+  state = {
+    counter: 0
+  };
+
+  handleClick = e => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  render() {
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand as={Link} to="/">
+          ReactJS
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/counter">
+              Contador
+            </Nav.Link>
+            <Nav.Link as={Link} to="/todos">
+              Tarefas
+            </Nav.Link>
+            <Nav.Link as={Link} to="/text">
+              Editor de texto
+            </Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link onClick={this.handleClick}>
+              Notificações{" "}
+              <Badge variant="light">
+                {this.state.counter ? this.state.counter : ""}
+              </Badge>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
