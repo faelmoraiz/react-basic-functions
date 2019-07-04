6e3523cb7447fad70c556c8e4cb3f79bcf8ccda5
faelmoraiz <faelmoraiz@gmail.com>
2019-07-04 09:31:58 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
-import Header from "./components/Header";
+import Header from "./components/Header/indexBootstrap";
 import Routes from "./routes";
 
 function App() {
